07264eda3ef1df0db5656b605ca22026a7d10343
RahafN1 <rahafalabdalh24@gmail.com>
2026-07-14 17:15:15 +0300

    Add amenities namespace to API

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
 
 
 def create_app():
@@ -9,5 +9,6 @@ def create_app():
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
     return app
