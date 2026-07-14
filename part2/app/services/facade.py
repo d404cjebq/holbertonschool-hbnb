1885c0021c5d8c6d342746ebb388d480fa7584d9
RahafN1 <rahafalabdalh24@gmail.com>
2026-07-14 17:16:28 +0300

    Add amenity management methods to HBnBFacade

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity
 
 
 class HBnBFacade:
@@ -33,6 +34,24 @@ class HBnBFacade:
 
         self.user_repo.update(user_id, user_data)
         return user
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return amenity
 
     def get_place(self, place_id):
         return self.place_repo.get(place_id)
