1ccdd3365207aecbfefa9e986ac3fa071c430cca
d404cjebq <imdhaydhw@gmail.com>
2026-07-30 17:41:37 +0300

    Part 3 - Task 5: Update SQLAlchemyRepository (structure only, no DB init yet)

--- part3/config.py
+++ part3/config.py
@@ -4,12 +4,12 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///development.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 config = {
