0c9b622b61f397faf7d57b32eff94b3b29d44dab
RahafN1 <rahafalabdalh24@gmail.com>
2026-08-02 16:28:37 +0300

    Refactor BaseModel to use SQLAlchemy ORM

--- part3/app/models/base_model.py
+++ part3/app/models/base_model.py
@@ -1,16 +1,18 @@
+from app import db
 import uuid
 from datetime import datetime
 
+class BaseModel(db.Model):
+    __abstract__ = True
 
-class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def save(self):
         """Update the updated_at timestamp whenever the object is modified"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
+        db.session.commit()
 
     def update(self, data):
         """Update the attributes of the object based on the provided dictionary"""
@@ -18,11 +20,3 @@ class BaseModel:
             if hasattr(self, key):
                 setattr(self, key, value)
         self.save()
-
-    def delete(self):
-        """Mark object for deletion (actual removal handled by repository)"""
-        pass
-
-    def create(self):
-        """Placeholder for creation logic (handled by repository/facade)"""
-        pass
