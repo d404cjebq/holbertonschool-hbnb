fa2d0429282533f93ad2ff0a442ae862af90b84c
RahafN1 <rahafalabdalh24@gmail.com>
2026-08-02 16:58:28 +0300

    Task 6: Map User entity to SQLAlchemy model with UserRepository

--- part3/app/services/facade.py
+++ part3/app/services/facade.py
@@ -1,4 +1,5 @@
-﻿from app.persistence.repository import InMemoryRepository, SQLAlchemyRepository
+from app.persistence.repository import InMemoryRepository
+from app.services.repositories.user_repository import UserRepository
 from app.models.user import User
 from app.models.amenity import Amenity
 from app.models.place import Place
@@ -7,7 +8,32 @@ from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = SQLAlchemyRepository(User)
+        self.user_repo = UserRepository()
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
+
+    def create_user(self, user_data):
+        password = user_data.pop('password', None)
+        user = User(**user_data)
+        if password:
+            user.hash_password(password)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_user_by_email(email)
+
+    def get_all_users(self):
+        return self.user_repo.get_all()
+
+    def update_user(self, user_id, user_data):
+        if 'password' in user_data:
+            user = self.get_user(user_id)
+            if user:
+                user.hash_password(user_data.pop('password'))
+        self.user_repo.update(user_id, user_data)
+        return self.user_repo.get(user_id)
