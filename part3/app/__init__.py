7aeee0d5ad5c80897c478896cbd360bfec5ee8cb
RahafN1 <rahafalabdalh24@gmail.com>
2026-08-03 13:30:53 +0300

    Reorganize import statements for API namespaces

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -3,11 +3,6 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.auth import api as auth_ns
 from config import config
 
 bcrypt = Bcrypt()
@@ -22,16 +17,20 @@ def create_app(config_class=config['default']):
     """
     app = Flask(__name__)
     app.config.from_object(config_class)
-
     app.config['JWT_SECRET_KEY'] = app.config['SECRET_KEY']
 
     bcrypt.init_app(app)
     jwt.init_app(app)
     db.init_app(app)
 
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.auth import api as auth_ns
+
     api = Api(app, version='1.0', title='HBnB API',
               description='HBnB Application API', doc='/api/v1/')
-
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
