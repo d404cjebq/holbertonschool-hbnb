b75b9d19142f449c759f0e59d320ecdf1de6297f
RahafN1 <rahafalabdalh24@gmail.com>
2026-08-05 13:08:50 +0300

    Make password field mandatory in User model

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -10,7 +10,7 @@ class User(BaseModel):
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     birth_date = db.Column(db.Date, nullable=True)
-    password = db.Column(db.String(128), nullable=True)
+    password = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     is_active = db.Column(db.Boolean, default=True)
 
