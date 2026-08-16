38662191e5978233e633ccb3be7a567b3c1733ea
SarahAlkhubaizy <sarahalkhubaizy@gmail.com>
2026-08-16 17:16:58 +0300

    "Part 4 - Task 1: Complete login functionality - JWT stored in cookie, tested with valid/invalid credentials, redirects to index.html"

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -3,38 +3,44 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from config import config
 
-bcrypt = Bcrypt()
 jwt = JWTManager()
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
-    """
-    Application Factory function.
-    Creates and configures the Flask app using the specified configuration.
-    """
     app = Flask(__name__)
     app.config.from_object(config_class)
-    app.config['JWT_SECRET_KEY'] = app.config['SECRET_KEY']
+
+    CORS(app)
+
+    api = Api(app, version='1.0', title='HBnB API',
+              description='HBnB Application API', doc='/api/v1/')
 
     bcrypt.init_app(app)
     jwt.init_app(app)
     db.init_app(app)
 
     from app.api.v1.users import api as users_ns
-    from app.api.v1.amenities import api as amenities_ns
-    from app.api.v1.places import api as places_ns
-    from app.api.v1.reviews import api as reviews_ns
-    from app.api.v1.auth import api as auth_ns
-
-    api = Api(app, version='1.0', title='HBnB API',
-              description='HBnB Application API', doc='/api/v1/')
     api.add_namespace(users_ns, path='/api/v1/users')
+    from app.api.v1.amenities import api as amenities_ns
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    from app.api.v1.places import api as places_ns
     api.add_namespace(places_ns, path='/api/v1/places')
+    from app.api.v1.reviews import api as reviews_ns
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    from app.api.v1.auth import api as auth_ns
     api.add_namespace(auth_ns, path='/api/v1/auth')
+
+    with app.app_context():
+        from app.models.user import User
+        from app.models.place import Place
+        from app.models.review import Review
+        from app.models.amenity import Amenity
+
+        db.create_all()
 
     return app
--- part3/app/models/place_amenity.py
+++
@@ -1,8 +0,0 @@
-from app.models.base_model import BaseModel
-
-
-class PlaceAmenity(BaseModel):
-    def __init__(self, place_id, amenity_id):
-        super().__init__()
-        self.place_id = place_id
-        self.amenity_id = amenity_id
