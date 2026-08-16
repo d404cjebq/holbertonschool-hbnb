a7d53551110bd3b05839f0dfdff53df98abf181d
d404cjebq <imdhaydhw@gmail.com>
2026-08-16 15:58:19 +0300

    Add Design task

--- part4/scripts.js
+++ part4/scripts.js
@@ -2,7 +2,6 @@
   This is a SAMPLE FILE to get you started.
   Please, follow the project instructions to complete the tasks.
 */
-
 document.addEventListener('DOMContentLoaded', () => {
     /* DO SOMETHING */
   });
